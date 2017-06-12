992f95a3023e83bfeb89f2bfbb73e44a298eaa05
whasting <hastingswade1@gmail.com>
2017-06-12 15:13:58 -0700

    setup message echo

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -9,9 +9,30 @@ class BotController < ApplicationController
 
   def receive_message
     if params[:entry]
-      render text: params[:entry]
-      # message = params['entry'].first['messaging']['message']['text']
-      # render text: "Your message was: #{message}"
+      message_data = params[:entry].first[:messaging].first
+      sender = message_data[:sender]
+      send_message(sender, message_data)
     end
+  end
+
+  def send_message(sender_id, message_data)
+    message = "Your message: #{message_data[:message][:text]}"
+
+    body = {
+      recipient: {
+        id: sender_id
+      },
+      message: {
+        text: message
+      }
+    }.to_json
+
+    response = HTTParty.post(
+      "https://graph.facebook.com/v2.6/me/messages?access_token=#{mysecretverifytoken}",
+      body: body,
+      headers: {
+        'Content-Type' => 'application/json'
+      }
+    )
   end
 end
